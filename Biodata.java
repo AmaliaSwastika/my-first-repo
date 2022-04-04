e772daa2b019e04a5bd50bd21b1b0d4667147079
AmaliaSwastika <102946499+AmaliaSwastika@users.noreply.github.com>
2022-04-04 15:42:35 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Asus
+ */
+public class Biodata {
+    public void getFullName(String fullName){
+        System.out.println("Nama Lengkap = "+fullName);
+    }
+    public void getPlaceBirth(String place){
+        System.out.println("Tempat Lahir = "+place);
+    }
+    public void getDateBirth(int x,String date,int z){
+        System.out.println("Tanggal Lahir = "+x+""+date+""+z);
+    }
+    public void getHobby(String hobby){
+        System.out.println("Hobi = "+hobby);
+    }
+    public void getZodiac(String Zodiac){
+        System.out.println("Zodiac =  "+Zodiac);
+    }
+    public void getFavFood(String FavFood){
+        System.out.println("Makanan Favorit = "+FavFood);
+    }
+    public void getDreams(String Dreams){
+        System.out.println("Cita-cita = "+Dreams); 
+    }
+    
+}
